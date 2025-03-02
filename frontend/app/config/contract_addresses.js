de1ebe618384f5ae98b37a497f31a5e5629c9abf
Stephanie Nguyen <tiennguyen.nltt@gmail.com>
2025-03-01 23:45:43 -0500

    Update contract addresses

--- frontend/app/config/contract_addresses.js
+++ frontend/app/config/contract_addresses.js
@@ -1,26 +1,26 @@
 export const config = {
-    "31337": {
-        "factory": {
-            "address": "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-        },
-        "nativeLiquidityPool": {
-            "address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-        }
+  31337: {
+    factory: {
+      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
     },
-    "57054": {
-        "factory": {
-            "address": "0x53083e4c228Dad830791a4D41eD4e0b0Ec8B7717"
-        },
-        "nativeLiquidityPool": {
-            "address": "0x9ffe5C21D89C7e685175381aCE1b6381BC093297"
-        }
+    nativeLiquidityPool: {
+      address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
     },
-    "1313161555": {
-        "factory": {
-            "address": "0x7aB5cEee0Ff304b053CE1F67d84C33F0ff407a55"
-        },
-        "nativeLiquidityPool": {
-            "address": "0x26Df0Ea798971A97Ae121514B32999DfDb220e1f"
-        }
-    }
-}
+  },
+  57054: {
+    factory: {
+      address: "0x53083e4c228Dad830791a4D41eD4e0b0Ec8B7717",
+    },
+    nativeLiquidityPool: {
+      address: "0x9ffe5C21D89C7e685175381aCE1b6381BC093297",
+    },
+  },
+  1313161555: {
+    factory: {
+      address: "0xB8d6D6b01bFe81784BE46e5771eF017Fa3c906d8",
+    },
+    nativeLiquidityPool: {
+      address: "0xf524930660f75CF602e909C15528d58459AB2A56",
+    },
+  },
+};
